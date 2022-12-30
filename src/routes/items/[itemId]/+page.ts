8e727274ab5d0062315e9917d94e86bfa91b5205
Joorrit <Jorrit.Krumrein@gmx.de>
2022-12-30 15:11:28 +0100

    exchange rate fixed

--- src/routes/items/[itemId]/+page.ts
+++ src/routes/items/[itemId]/+page.ts
@@ -1,3 +1,4 @@
+import { exchangeRate } from '$lib/functions/stores';
 import type { ItemPriceHistoryRes, PositionInformationEntry } from '$lib/functions/types';
 import type { PageLoad } from './$types';
 
@@ -8,8 +9,16 @@ export const load = (async ({ params }: any) => {
 		`https://joorrit.de/api/items/${params.itemId}/price_history`
 	);
 	const itemPriceHistoryFetchedData: ItemPriceHistoryRes = await itemPriceHistoryRes.json();
-	const itemPositionInformationRes = await fetch(`https://joorrit.de/api/inventory/positions_information/${params.itemId}`);
-	const itemPositionInformationFetchedData: PositionInformationEntry = await itemPositionInformationRes.json();
+	const itemPositionInformationRes = await fetch(
+		`https://joorrit.de/api/inventory/positions_information/${params.itemId}`
+	);
+	const itemPositionInformationFetchedData: PositionInformationEntry =
+		await itemPositionInformationRes.json();
+
+	const exchangeRateRes = await fetch('https://joorrit.de/api/exchange_rate');
+	const exchangeRateFetchedData = await exchangeRateRes.json();
+	const exchangeRateVal = exchangeRateFetchedData.data;
+	exchangeRate.set(exchangeRateVal);
 
 	return {
 		itemPriceHistory: itemPriceHistoryFetchedData,
